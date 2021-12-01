067614b29d60654c7b1ee2717638e7dcf90217ab
Vu Hoang Phuc <vuhoangphuc272@gmail.com>
2021-12-01 19:42:56 +0700

    change id position to array

--- position.js
+++ position.js
@@ -1,27 +1,37 @@
 const position = [
   {
     name: "rookBlack",
-    id: "a8",
-    link: "asset/rook_black.png"
-  },
-  {
-    name: "rookBlack",
-    id:  "h8",
+    id: ["a8", "h8"],
     link: "asset/rook_black.png"
   },
   {
     name: "rookWhite",
-    id: "a1",
+    id: ["a1", "h1"],
     link: "asset/rook_white.png"
   },
   {
-    name: "rookWhite",
-    id: "h1",
-    link: "asset/rook_white.png"
-  }
+    name: "pawnBlack",
+    id: ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"],
+    link: "asset/pawn_black.png"
+  },
+  {
+    name: "pawnWhite",
+    id: ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"],
+    link: "asset/pawn_white.png"
+  },
+  {
+    name: "knightBlack",
+    id: ["b8", "g8"],
+    link: "asset/knight_black.png"
+  },
+  {
+    name: "knightWhite",
+    id: ["b1", "g1"],
+    link: "asset/knight_white.png"
+  },
 ]
 
-function creep(id, url) {
+function setCreep(id, url) {
   const square = document.getElementById(id);
   const img = document.createElement("img")
   img.src = url
@@ -30,5 +40,6 @@ function creep(id, url) {
 
 
 position.forEach((item, index) => {
-  creep(item.id, item.link)
+  item.id.forEach(creep => setCreep(creep, item.link))
+  
 })
