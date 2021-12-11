360e9ee7d1ef5d4b828678d394d36a44dfa4fe23
Tus Nguyen <tusnguyen1805@gmail.com>
2021-12-12 01:16:42 +0700

    recommend all 1st move

--- pieces/king.js
+++ pieces/king.js
@@ -2,4 +2,35 @@ function King(isWhite, id, key){
   const kingUrl = isWhite ? "asset/king_white.png" : "asset/king_black.png"
   this.isWhite = isWhite;
   ChessPiece.call(this, "king", id, kingUrl, key);
+  this.recommendMoves = function (id) {
+    let x = id.charAt(0);
+    let y = parseInt(id.charAt(1));
+    x = column.findIndex((value) => value === x);
+    // console.log(x, y);
+    board.resetSquareColor();
+    if (this.isEmpty(`${column[x + 1]}${y}`)) {
+      board.changeSquareColor(x + 1, y, this);
+    }
+    if (this.isEmpty(`${column[x + 1]}${y + 1}`)) {
+      board.changeSquareColor(x + 1, y + 1, this);
+    }
+    if (this.isEmpty(`${column[x]}${y + 1}`)) {
+      board.changeSquareColor(x, y + 1, this);
+    }
+    if (this.isEmpty(`${column[x - 1]}${y}`)) {
+      board.changeSquareColor(x - 1, y, this);
+    }
+    if (this.isEmpty(`${column[x - 1]}${y - 1}`)) {
+      board.changeSquareColor(x - 1, y - 1, this);
+    }
+    if (this.isEmpty(`${column[x]}${y - 1}`)) {
+      board.changeSquareColor(x, y - 1, this);
+    }
+    if (this.isEmpty(`${column[x + 1]}${y - 1}`)) {
+      board.changeSquareColor(x + 1, y - 1, this);
+    }
+    if (this.isEmpty(`${column[x - 1]}${y + 1}`)) {
+      board.changeSquareColor(x - 1, y + 1, this);
+    }
+  }
 }
--- pieces/knight.js
+++ pieces/knight.js
@@ -8,5 +8,33 @@ function Knight(isWhite, id, key){
     x = column.findIndex((value) => value === x);
     // console.log(x, y);
     board.resetSquareColor();
+    //top right
+    if (this.isEmpty(`${column[x + 1]}${y + 2}`)) {
+      board.changeSquareColor(x + 1, y + 2, this);
+    }
+    if (this.isEmpty(`${column[x + 2]}${y + 1}`)) {
+      board.changeSquareColor(x + 2, y + 1, this);
+    }
+    //top left
+    if (this.isEmpty(`${column[x - 1]}${y + 2}`)) {
+      board.changeSquareColor(x - 1, y + 2, this);
+    }
+    if (this.isEmpty(`${column[x - 2]}${y + 1}`)) {
+      board.changeSquareColor(x - 2, y + 1, this);
+    }
+    //bottom right
+    if (this.isEmpty(`${column[x + 1]}${y - 2}`)) {
+      board.changeSquareColor(x + 1, y - 2, this);
+    }
+    if (this.isEmpty(`${column[x + 2]}${y - 1}`)) {
+      board.changeSquareColor(x + 2, y - 1, this);
+    }
+    // bottom left
+    if (this.isEmpty(`${column[x - 1]}${y - 2}`)) {
+      board.changeSquareColor(x - 1, y - 2, this);
+    }
+    if (this.isEmpty(`${column[x - 2]}${y - 1}`)) {
+      board.changeSquareColor(x - 2, y - 1, this);
+    }
   }
 }
