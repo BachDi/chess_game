2e68557655877d14c81a34979d4bf34ff81bf873
Vu Hoang Phuc <vuhoangphuc272@gmail.com>
2021-12-28 22:11:57 +0700

    Fix 1 xíu

--- API/index.js
+++ API/index.js
@@ -1,8 +1,8 @@
 const http = require("http");
-const fs = require("fs");
-const tasks = require("./tasks.json");
-const app = require("./app");
-const routes = require("./routes/task")
+// const fs = require("fs");
+// const tasks = require("./tasks.json");
+// const app = require("./app");
+const routes = require("./routes/routes");
 
 const hostname = "127.0.0.1";
 const port = 3000;
@@ -13,7 +13,9 @@ const server = http.createServer((req, res) => {
   const routeUrl = req.url.split("?")[0];
   const method = req.method;
   const router = getRouter(req);
+  console.log(router);
   const handler = router[method][routeUrl];
+  console.log(handler);
   return handler(req, res);
 });
 
@@ -23,6 +25,7 @@ server.listen(port, hostname, () => {
 
 function getRouter(req) {
   const routeUrl = req.url.split("?")[0];
+  console.log(routeUrl);
 
   switch (routeUrl) {
     case "/tasks":
