044e4e29951f9e5a1311f8367bfb0c30a1870af4
Vu Hoang Phuc <vuhoangphuc272@gmail.com>
2022-01-21 20:47:36 +0700

    webpack

---
+++ webpack/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  // entry: "./src/index.js",
+  // entry: {
+  //   main: "./src/index.js",
+  // },
+  // entry: ["./src/index.js", "./src/abc.js"],
+  entry: {
+    index2: "./src/index.js",
+    abc2: "./src/abc.js",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "[name].js",
+    // filname: "bundle.js",
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
+    hotOnly: true,
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+};
